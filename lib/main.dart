03c8fbbe3d3357283fd53220b92316c898c9d69c
Juan <j.sanchezala@gmail.com>
2021-06-16 17:37:41 +0200

    Create lateral menu and the login system

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,63 @@
 import 'package:animalspredictor/splashscreen.dart';
+import 'package:animalspredictor/ui/splash.dart';
 import 'package:flutter/material.dart';
+import 'authentication_service.dart';
+import 'ui/home.dart';
+import 'ui/sign_in.dart';
+import 'ui/sign_up.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Animals Predictor',
-      home: MySplash(),
-      debugShowCheckedModeBanner: false,
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<AuthenticationService>().authStateChanges,
+          initialData: null,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Firebase Auth',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            primaryColor: Colors.indigoAccent
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Splash(),
+          '/auth': (context) => AuthenticationWrapper(),
+          '/signin': (context) => SignIn(),
+          '/signup': (context) => SignUp(),
+          '/home': (context) => Home(),
+        },
+      ),
     );
+  }
+}
+
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseuser = context.watch<User>();
+    if (firebaseuser != null) {
+      return Home();
+    }
+    return SignIn();
   }
 }
